f0b29e69299d34deba0ef35eff5e215bc6d7147f
Terry Feng <tzfeng@ucsd.edu>
2020-12-13 23:41:23 -0800

    completion

--- attention.sh
+++ attention.sh
@@ -5,7 +5,7 @@ IN_PORT="magenta_in"
 OUT_PORT="magenta_out"
 echo "MIDI IN: $IN_PORT"
 echo "MIDI OUT: $OUT_PORT"
-BUNDLE_PATHS="./magenta_models/attention.mag"
+BUNDLE_PATHS="./magenta_models/attention_rnn.mag"
 
 magenta_midi \
 	 --input_ports=${IN_PORT} \
